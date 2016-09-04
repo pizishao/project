af0de1162c1fdcfb13805a9c45b98cac74f43056
unknown <410017376@qq.com>
2016-09-04 11:44:30 +0800

    new day day up unity framwork

---
+++ DayDayUpFramework/Assets/Script/Utility/ThreadNotifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ThreadNotifier
+{
+    Queue<Action> actionQueue = new Queue<Action>();
+
+    void Post(Action task)
+    {
+        lock (actionQueue)
+        {
+            actionQueue.Enqueue(task);
+        }
+    }
+
+    void Dispatch()
+    {
+        lock (actionQueue)
+        {
+            while (actionQueue.Count > 0)
+            {
+                var task = actionQueue.Dequeue();
+                if (task != null)
+                {
+                    try
+                    {
+                        task();                           
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogError(ex.ToString());
+                    }
+                }
+            }            
+        }
+    }
+}
